8c9709c3367eefc77b6c3849d52b8295df33cfdf
Maxime <85428733+maax6@users.noreply.github.com>
2023-02-23 22:59:17 +0100

    🧲 Display data via json

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,46 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./sass/App.scss";
+import membersData from './assets/members.json'
+import './sass/App.scss'
 
 function App() {
- const [count, setCount] = useState(0);
-
- return (
-  <div className="App">
-   <header className="header">
-    <h1 className="header__h1" style={{ fontFamily: "PT Serif", color: "#000000" }}>
-     The creative crew
-    </h1>
-    <div className=" header__texts">
-     <h3 className="header__texts__h3" style={{ fontFamily: "Poppins" }}>
-      WHO WE ARE
-     </h3>
-     <p className="header__texts__p">
-      We are team of creatively diverse. driven. innovative individuals working
-      in various locations from the world.
-     </p>
-    </div>
-   </header>
-  </div>
- );
+   const members = membersData.members;
+   console.log(members);
+   const memberCards = members.map((member, index) => (
+      <div className="member-card" key={index}>
+         <p className="member-card__position">{member.position}</p>
+         <img
+            src={member.photo}
+            alt={member.name}
+            className="member-card__photo"
+         />
+         <figcaption className="member-card__name">{member.name}</figcaption>
+      </div>
+   ))
+   
+   return (
+      <div className="App">
+         <header className="header">
+            <h1
+               className="header__h1"
+               style={{ fontFamily: 'PT Serif', color: '#000000' }}
+            >
+               The creative crew
+            </h1>
+            <div className="header__texts">
+               <h3
+                  className="header__texts__h3"
+                  style={{ fontFamily: 'Poppins' }}
+               >
+                  WHO WE ARE
+               </h3>
+               <p className="header__texts__p">
+                  We are team of creatively diverse. driven. innovative
+                  individuals working in various locations from the world.
+               </p>
+            </div>
+         </header>
+         <div className="member-cards">{memberCards}</div>
+      </div>
+   )
 }
 
-export default App;
+export default App
